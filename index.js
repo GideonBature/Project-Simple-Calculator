7de9c22c80c5bfc8213d09bf3eae08692833339e
GideonBature <infoaboutgideon@gmail.com>
2022-11-08 00:00:02 +0100

    add event listener to buttons and update display with it

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 const buttons = document.querySelectorAll(".btn-operator, .btn-number");
 const display = document.querySelector(".display");
+let displayData = "";
 
-display.textContent = "2";
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        const buttonValue = button.getAttribute('data-set');
+        
+        displayData += buttonValue;
+
+        display.textContent = displayData;
+    })
+})
