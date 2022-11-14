092107997ce936d7a0320f25eea49f11c5f7b9c3
GideonBature <infoaboutgideon@gmail.com>
2022-11-14 19:05:51 +0100

    add the operator and the number and update the display

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//get the buttons
+const display = document.querySelector('.screen')
 const numbers = document.querySelectorAll('.btn-number');
 const operators = document.querySelectorAll('.btn-operator');
 const clear = document.querySelector('.btn-clear');
@@ -6,3 +8,33 @@ const percentage = document.querySelector('.btn-percentage');
 const del = document.querySelector('.btn-delete');
 const point = document.querySelector('.btn-point');
 const equals = document.querySelector('btn-equals');
+
+//define values
+let displayValue = "";
+let num1 = "";
+let num2 = "";
+
+//add buttons event listeners
+numbers.forEach(button => {
+    button.addEventListener('click', (e) => {
+        console.log(e.target.dataset.set)
+        const buttonValue = e.target.dataset.set;
+        num1 += buttonValue;
+        display.textContent = num1;
+        
+        
+
+    })
+})
+
+operators.forEach(button => {
+    button.addEventListener('click', (e) => {
+        console.log(e.target.dataset.set)
+        const operatorValue = e.target.dataset.set;
+        num1 = num2;
+        num2 = "";
+        display.textContent = "";
+
+    })
+})
+
