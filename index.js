7d971d6ddaa22a6d88b1566d9fb1d78ee99d8605
GideonBature <infoaboutgideon@gmail.com>
2022-11-14 22:47:17 +0100

    operators and operands working perfectly

--- index.js
+++ index.js
@@ -7,34 +7,81 @@ const power = document.querySelector('.btn-power');
 const percentage = document.querySelector('.btn-percentage');
 const del = document.querySelector('.btn-delete');
 const point = document.querySelector('.btn-point');
-const equals = document.querySelector('btn-equals');
+const equal = document.querySelector('.btn-equals');
 
 //define values
 let displayValue = "";
 let num1 = "";
 let num2 = "";
+let operator = "";
 
 //add buttons event listeners
 numbers.forEach(button => {
     button.addEventListener('click', (e) => {
-        console.log(e.target.dataset.set)
         const buttonValue = e.target.dataset.set;
-        num1 += buttonValue;
-        display.textContent = num1;
-        
-        
+        if(num1.length < 30) {
+            num1 += buttonValue;
+            display.textContent = num1;
+        }
 
     })
 })
 
 operators.forEach(button => {
     button.addEventListener('click', (e) => {
-        console.log(e.target.dataset.set)
         const operatorValue = e.target.dataset.set;
-        num1 = num2;
-        num2 = "";
-        display.textContent = "";
+        operator = operatorValue;
+        num2 = num1;
+        num1 = "";
+        display.textContent = num2;
 
     })
 })
 
+clear.addEventListener('click', () => {
+    num1 = '';
+    num2 = '';
+    operator = '';
+    display.textContent = '';
+})
+
+equal.addEventListener('click', () => {
+    num1 = Number(num1);
+    num2 = Number(num2);
+
+    const add = function(n1, n2) {
+        return (n1+n2);
+    }
+    const subtract = function(n1, n2) {
+        return n2 - n1;
+    }
+    const multiply = function(n1, n2) {
+        return n1 * n2;
+    }
+    const divide = function(n1, n2) {
+        if(n1 == 0) {
+            return "Infinity"
+        } else {
+            return n2/n1;
+        }
+    }
+
+    if(operator == "+") {
+        let result = add(num1, num2);
+        display.textContent = result;
+        return result;
+        
+    } else if (operator == "-") {
+        let result = subtract(num1, num2);
+        display.textContent = result;
+        return result;
+    } else if (operator == "*") {
+        let result = multiply(num1, num2);
+        display.textContent = result;
+        return result;
+    } else if (operator == "/") {
+        let result = divide(num1, num2);
+        display.textContent = result;
+        return result;
+    }
+})
