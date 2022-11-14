2a61a201887444dbcefa4175b0488a6b8bc9d015
GideonBature <infoaboutgideon@gmail.com>
2022-11-14 01:08:10 +0100

    get buttons using query selector

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-
+const numbers = document.querySelectorAll('.btn-number');
+const operators = document.querySelectorAll('.btn-operator');
+const clear = document.querySelector('.btn-clear');
+const power = document.querySelector('.btn-power');
+const percentage = document.querySelector('.btn-percentage');
+const del = document.querySelector('.btn-delete');
+const point = document.querySelector('.btn-point');
+const equals = document.querySelector('btn-equals');
