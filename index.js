28f86d2077889244797b3af7c8c26401edf283b1
GideonBature <infoaboutgideon@gmail.com>
2022-11-15 07:07:01 +0100

    the power, percentage, delete and decimal point operators all working perfectly

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const display = document.querySelector('.screen')
 const numbers = document.querySelectorAll('.btn-number');
 const operators = document.querySelectorAll('.btn-operator');
 const clear = document.querySelector('.btn-clear');
-const power = document.querySelector('.btn-power');
-const percentage = document.querySelector('.btn-percentage');
+const percent = document.querySelector('.btn-percentage');
 const del = document.querySelector('.btn-delete');
 const point = document.querySelector('.btn-point');
 const equal = document.querySelector('.btn-equals');
@@ -45,12 +44,13 @@ clear.addEventListener('click', () => {
     display.textContent = '';
 })
 
-equal.addEventListener('click', () => {
+const calculate = function() {
+
     num1 = Number(num1);
     num2 = Number(num2);
 
     const add = function(n1, n2) {
-        return (n1+n2);
+        return n1 + n2;
     }
     const subtract = function(n1, n2) {
         return n2 - n1;
@@ -65,23 +65,58 @@ equal.addEventListener('click', () => {
             return n2/n1;
         }
     }
+    const power = function(n1, n2) {
+        return n2 ** n1;
+    }
 
     if(operator == "+") {
         let result = add(num1, num2);
+        num1 = result;
         display.textContent = result;
         return result;
         
     } else if (operator == "-") {
         let result = subtract(num1, num2);
+        num1 = result;
         display.textContent = result;
         return result;
     } else if (operator == "*") {
         let result = multiply(num1, num2);
+        num1 = result;
         display.textContent = result;
         return result;
     } else if (operator == "/") {
         let result = divide(num1, num2);
+        num1 = result;
+        display.textContent = result;
+        return result;
+    } else if (operator == "^") {
+        let result = power(num1, num2);
+        num1 = result;
         display.textContent = result;
         return result;
     }
+
+}
+
+equal.addEventListener('click', () => {
+
+    calculate();
+    
+})
+
+point.addEventListener('click', () => {
+    if(!num1.includes('.')) {
+        num1 += '.';
+    }
+})
+
+del.addEventListener('click', ()=> {
+    num1 = num1.slice(0, -1);
+    display.textContent = num1;
+})
+
+percent.addEventListener('click', () => {
+    num1 = num1/100;
+    display.textContent = num1;
 })
